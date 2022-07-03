e08ec12df5a61f42ef21d57957e178ac4934f342
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-03 18:47:37 -0300

    feat: dev VSL comp

--- src/modules/VSL/index.jsx
+++ src/modules/VSL/index.jsx
@@ -1,9 +1,68 @@
-import { ContainerStyle, Video } from './styles'
+import { useState, useEffect } from 'react'
+
+import Gif from '../../assets/gif.gif'
+
+import Volume from './SVG'
+import { ContainerStyle, Player } from './styles'
 
 export default function VSL () {
   return (
     <ContainerStyle>
-      <Video/>
+      <Player>
+        <Film/>
+        <AudioButton/>
+        <Thumbnail/>
+        <Video/>
+      </Player>
     </ContainerStyle>
+  )
+}
+
+function Film () {
+  return (
+    <div
+      className="film"
+      onClick={() => console.log('play/pause video')}
+    >
+    </div>
+  )
+}
+
+function Video () {
+  return (
+    <iframe
+      className="video"
+      src={process.env.URL_VIDEO}
+      frameborder="0"
+    >
+    </iframe>
+  )
+}
+
+function Thumbnail () {
+  return (
+    <img
+      className='thumbnail'
+      src={Gif}
+    />
+  )
+}
+
+function AudioButton () {
+  const [interaction, setInteraction] = useState('click')
+
+  useEffect(() => {
+    if (window.innerWidth <= 768)
+      setInteraction('touch')
+  }, [])
+
+  return (
+    <div className="button">
+      <div>
+        <span>O vídeo já começou,</span>
+        <Volume/>
+        <span>{interaction === 'click' ? 'Click' : 'Toque'} para ouvir.</span>
+      </div>
+    </div>
   )
 }
--- src/modules/VSL/styles.js
+++ src/modules/VSL/styles.js
@@ -1,11 +1,140 @@
 import styled from 'styled-components'
 
 export const ContainerStyle = styled.div`
+  width: 100%;
+  max-width: 1440px;
+  padding: 0 6rem;
+
+
+  // tablet props
+  @media (max-width: 768px) {
+    padding: 0 4rem;
+  }
+
+  // mobile props
+  @media (max-width: 425px) {
+    padding: 0 1.5rem;
+  }
 
 `
 
-export const Video = styled.div`
-  width: calc(800vw / 9);
-  height: calc(800vw / 16);
-  background-color: var(--background-card-color);
+export const Player = styled.div`
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 */
+  border-radius: 0.5rem;
+  border: 2px solid var(--primary);
+  height: 0;
+
+  // tablet props
+  @media (max-width: 768px) {
+    border-radius: 0.4rem;
+  }
+
+  // mobile props
+  @media (max-width: 425px) {
+    border-radius: 0.3rem;
+  }
+
+  .film {
+    border-radius: inherit;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 100;
+  }
+
+  .button {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 101;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    div {
+      font-size: 1.2rem;
+      color: var(--white-text);
+      background-color: var(--primary);
+      border-radius: 0.3rem;
+      padding: 1.5rem;
+      opacity: 0.9;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+
+    }
+
+    svg {
+      border-radius: inherit;
+      width: 60px;
+      height: 47px;
+    }
+
+    @-webkit-keyframes BLINK {
+      0% { opacity: 0; }
+      33% { opacity: 1; }
+      66% { opacity: 1; }
+      100% { opacity: 0; }
+    }
+
+    @keyframes BLINK {
+      0% { opacity: 0; }
+      33% { opacity: 1; }
+      66% { opacity: 1; }
+      100% { opacity: 0; }
+    }
+
+    .blink_1 {
+      -webkit-animation: BLINK 1s infinite;
+      animation: BLINK 1s infinite;
+      opacity: 0;
+    }
+
+    .blink_2 {
+      -webkit-animation: BLINK 1s infinite .15s;
+      animation: BLINK 1s infinite .15s;
+      opacity: 0;
+    }
+
+    .blink_3 {
+      -webkit-animation: BLINK 1s infinite .3s;
+      animation: BLINK 1s infinite .3s;
+      opacity: 0;
+    }
+
+    .svg-color {
+      fill: #FFFFFF;
+    }
+  }
+
+  .thumbnail {
+    border-radius: inherit;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+
+    filter: brightness(0.5);
+  }
+
+  .video {
+    border-radius: inherit;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--background-card-color);
+    z-index: 1;
+  }
 `
