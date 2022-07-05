e572a59e836f8dfc683ff9fe93d535f855608885
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-04 21:36:15 -0300

    feat: implements action comp

--- src/components/Action/index.jsx
+++ src/components/Action/index.jsx
@@ -1,9 +1,14 @@
 import { ContainerStyle } from './styles'
+import Download from '../../assets/download.svg'
 
 export default function Action () {
   return (
     <ContainerStyle>
-      Action
+      <div className="cover"></div>
+      <a href="">
+        <img src={Download}/>
+        <span>Baixar agora</span>
+      </a>
     </ContainerStyle>
   )
 }
--- src/components/Action/styles.js
+++ src/components/Action/styles.js
@@ -1,5 +1,48 @@
 import styled from 'styled-components'
 
 export const ContainerStyle = styled.div`
+  padding: 3rem;
+  position: relative;
 
+  // tablet props
+  @media (max-width: 768px) {
+    padding: 1.5rem;
+  }
+
+  // mobile props
+  @media (max-width: 425px) {
+    padding: 1rem;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    margin-top: 2rem;
+    padding: 1.3rem 2.8rem;
+    color: rgba(240,240,240,1);
+    background-color: var(--primary);
+    border-radius: 0.5rem;
+    transition: all 0.3s ease;
+    text-decoration: none;
+
+    span {
+      text-transform: uppercase;
+      font-size: 1rem;
+      font-weight: bold;
+    }
+
+    img {
+      width: 25px;
+      height: 25px;
+      margin-right: 0.6rem;
+      fill: #fff;
+    }
+
+    :hover {
+      color: rgba(255,255,255,1);
+      padding: 1.3rem 4rem;
+    }
+  }
 `
