95e968b78ea1c63e55ef0f3a73d63e0b7fb16fcc
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-05 21:52:10 -0300

    feat: implements action button

--- src/components/Action/index.jsx
+++ src/components/Action/index.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react'
 import { ContainerStyle } from './styles'
 import Download from '../../assets/download.svg'
 
 export default function Action () {
+  const [showButton, setShowButton] = useState(false)
+
   return (
     <ContainerStyle>
-      <div className="cover"></div>
-      <a href="">
-        <img src={Download}/>
-        <span>Baixar agora</span>
-      </a>
+      { showButton ?
+        <a href="">
+          <img src={Download}/>
+          <span>Baixar agora</span>
+        </a> : null
+      }
     </ContainerStyle>
   )
 }
--- src/components/Action/styles.js
+++ src/components/Action/styles.js
@@ -1,36 +1,37 @@
 import styled from 'styled-components'
 
 export const ContainerStyle = styled.div`
-  padding: 3rem;
+  padding: 4rem;
   position: relative;
 
   // tablet props
   @media (max-width: 768px) {
-    padding: 1.5rem;
+    padding: 3rem;
   }
 
   // mobile props
   @media (max-width: 425px) {
-    padding: 1rem;
+    padding: 2rem;
   }
 
   a {
     display: flex;
     align-items: center;
     justify-content: center;
-
-    margin-top: 2rem;
-    padding: 1.3rem 2.8rem;
     color: rgba(240,240,240,1);
     background-color: var(--primary);
     border-radius: 0.5rem;
     transition: all 0.3s ease;
     text-decoration: none;
 
+    padding: 1.3rem 2.8rem;
+    @media (max-width: 768px) { padding: 1.3rem 2.8rem; } // tablet props
+    @media (max-width: 425px) { padding: 1rem 2rem; } // mobile props
+
     span {
       text-transform: uppercase;
-      font-size: 1rem;
       font-weight: bold;
+      font-size: 1rem;
     }
 
     img {
@@ -43,6 +44,13 @@ export const ContainerStyle = styled.div`
     :hover {
       color: rgba(255,255,255,1);
       padding: 1.3rem 4rem;
+      animation: BOUNCE 1s ease-in-out infinite;
+
+      @keyframes BOUNCE {
+        0% { transform: scale(1); }
+        50% { transform: scale(1.08); }
+        100% { transform: scale(1); }
+      }
     }
   }
 `
