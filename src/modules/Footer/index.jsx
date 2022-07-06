3b81fa61607a87cb876ac0dd280af8119cc3d147
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-05 21:50:03 -0300

    feat: implements footer comp

--- src/modules/Footer/index.jsx
+++ src/modules/Footer/index.jsx
@@ -1,9 +1,30 @@
-import { FooterStyle } from './styles'
+import { ContainerStyle, FooterStyle } from './styles'
+import Logo from '../../assets/logo.png'
 
 export default function Footer () {
   return (
-    <FooterStyle>
-      Footer
-    </FooterStyle>
+    <ContainerStyle>
+      <FooterStyle>
+        <div>
+          <img src={Logo}/>
+          <span>Company Name 2022</span>
+          <span>Todos os direitos reservados</span>
+        </div>
+        <div>
+          <p>
+            Os produtos e serviços vendidos neste site não devem ser interpretados como uma promessa ou garantia de resultados.
+          </p>
+          <p>
+            Seu nível de sucesso em alcançar os resultados divulgados com o uso de nossos produtos e informações depende do tempo que você dedica ao programa, técnicas utilizadas, conhecimento e habilidades diferentes.
+          </p>
+          <p>
+            Como esses fatores diferem entre cada indivíduo, não podemos garantir o sucesso ou o nível de resultado, nem somos responsáveis por qualquer de suas ações.
+          </p>
+          <p>
+            Todas e quaisquer declarações prospectivas contidas neste site ou em qualquer um de nossos produtos destinam-se a expressar a nossa opinião sobre os resultados potenciais que algumas pessoas podem alcançar.
+          </p>
+        </div>
+      </FooterStyle>
+    </ContainerStyle>
   )
 }
--- src/modules/Footer/styles.js
+++ src/modules/Footer/styles.js
@@ -1,5 +1,63 @@
 import styled from 'styled-components'
 
-export const FooterStyle = styled.footer`
+export const ContainerStyle = styled.footer`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--background-side-color);
+    border-bottom: 1px solid var(--background-card-color);
+`
 
+export const FooterStyle = styled.footer`
+    width: 100%;
+    max-width: 1440px;
+    padding: 6rem;
+
+    display: flex;
+    flex-direction: row;
+
+    // tablet props
+    @media (max-width: 768px) {
+      padding: 4rem;
+      display: flex;
+      flex-direction: column;
+    }
+
+    // mobile props
+    @media (max-width: 425px) {
+      padding: 2rem;
+    }
+
+    div:first-child {
+      display: flex;
+      flex-direction: column;
+      margin: 0 4rem 0 0;
+      @media (max-width: 768px) { margin: 0 0 1rem 0; } // tablet props
+
+      img {
+        width: 250px;
+        margin-bottom: 1rem;
+        @media (max-width: 768px) { width: 160px; } // tablet props
+      }
+
+      span {
+        text-align: left;
+        margin-bottom: 0;
+      }
+    }
+
+
+
+    p, span {
+      text-align: justify;
+      margin-bottom: 1.5rem;
+      font-size: 0.8rem;
+
+      @media (max-width: 768px) { font-size: 0.7rem; margin-bottom: 1rem;  } // tablet props
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
 `
