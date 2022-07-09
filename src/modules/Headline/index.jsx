575ee8b41ac39ff6057fd8b3972ee5d511df7bb5
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-09 11:55:39 -0300

    refactor: improve headline component

--- src/modules/Headline/index.jsx
+++ src/modules/Headline/index.jsx
@@ -1,12 +1,12 @@
-import { HeaderStyle, TitleStyle } from './styles'
+import { HeadlineContainer, TitleStyle } from './styles'
 
 
 export default function Headline () {
   return (
-    <HeaderStyle>
+    <HeadlineContainer>
       <TitleStyle>
         Lorem ipsum dolor, <span>sit amet consectetur adipisicing</span> elit. Blanditiis modi quas magni. Harum excepturi impedit natus dolorum
       </TitleStyle>
-    </HeaderStyle>
+    </HeadlineContainer>
   )
 }
--- src/modules/Headline/styles.js
+++ src/modules/Headline/styles.js
@@ -1,42 +1,32 @@
 import styled from 'styled-components'
 
-export const HeaderStyle = styled.header`
-  width: 100%;
+export const HeadlineContainer = styled.div`
+  margin-top: 3rem;
+  @media (max-width: 768px) { margin-top: 4rem; } // tablet props
+  @media (max-width: 425px) { margin-top: 3rem; } // mobile props
+
   max-width: 1440px;
+  width: 100%;
+
   padding: 8rem;
-
-
-  // tablet props
-  @media (max-width: 768px) {
-    padding: 6rem 2rem;
-  }
-
-  // mobile props
-  @media (max-width: 425px) {
-    padding: 3rem 1.5rem;
-  }
+  @media (max-width: 768px) { padding: 4rem 3rem; } // tablet props
+  @media (max-width: 425px) { padding: 3rem 2rem; } // mobile props
 `
 
 export const TitleStyle = styled.h1`
+  font-size: 2.5rem;
+  @media (max-width: 768px) { font-size: 2rem; } // tablet props
+  @media (max-width: 425px) { font-size: 1.5rem; } // tablet props
+
   font-family: 'Lilita One', cursive;
   font-weight: 400;
+
   text-align: center;
+  @media (max-width: 425px) { text-align: left; } // mobile props
+
   color: var(--white-text);
-  font-size: 2.5rem;
 
   span {
     color: var(--primary);
-  }
-
-
-  // tablet props
-  @media (max-width: 768px) {
-    font-size: 2rem;
-  }
-
-  // mobile props
-  @media (max-width: 425px) {
-    font-size: 1.5rem;
-    text-align: left;
   }
 `
