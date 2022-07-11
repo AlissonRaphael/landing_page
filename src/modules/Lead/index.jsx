fe0345b5e0c29d96980675a0df22878fba2193d4
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-11 10:17:05 -0300

    refactor: improves lead component

--- src/modules/Lead/index.jsx
+++ src/modules/Lead/index.jsx
@@ -1,9 +1,27 @@
-import { ContainerStyle } from './styles'
+import SectionTitle from '../../components/SectionTitle'
+import Image2 from '../../assets/image2.jpg'
+
+import { LeadContainer, ImageSection, Article } from './styles'
 
 export default function Lead () {
   return (
-    <ContainerStyle>
-      Lead
-    </ContainerStyle>
+    <LeadContainer>
+      <ImageSection>
+        <img src={Image2}/>
+      </ImageSection>
+
+      <Article>
+        <h1>Antes de tudo, precisamos ser sinceros neste momento</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit distinctio consequuntur deleniti vero commodi quas facere ipsum dicta ad cupiditate voluptate quidem quasi iste, rem dolore in, eius quis nemo?
+          Quo modi cum repudiandae, autem assumenda earum, mollitia sequi doloremque dolor dolorem iusto nemo veritatis excepturi fugit.
+        </p>
+        <p>
+          Possimus soluta atque aspernatur obcaecati! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </p>
+      </Article>
+      <SectionTitle>sobre</SectionTitle>
+    </LeadContainer>
   )
 }
--- src/modules/Lead/styles.js
+++ src/modules/Lead/styles.js
@@ -1,5 +1,57 @@
 import styled from 'styled-components'
 
-export const ContainerStyle = styled.div`
+export const LeadContainer = styled.div`
+  /* margin-top: 2rem;
+  @media (max-width: 768px) { margin-top: 4rem; } // tablet props
+  @media (max-width: 425px) { margin-top: 3rem; } // mobile props */
 
+  width: 100%;
+
+  padding: 4rem;
+  @media (max-width: 768px) { padding: 4rem 3rem; } // tablet props
+  @media (max-width: 425px) { padding: 3rem 2rem; } // mobile props
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: relative;
+`
+
+export const ImageSection = styled.div`
+  margin-right: 8rem;
+  min-width: 35%;
+
+  position: relative;
+
+  img {
+    position: absolute;
+    top: -25rem;
+    left: -4rem;
+
+    height: 38rem;
+    width: 30rem;
+    background-color: var(--gray-300-text);
+  }
+`
+
+
+export const Article = styled.div`
+  padding: 0 4rem 0 0;
+  @media (max-width: 768px) { padding: 0 0 2rem 0; } // tablet props
+
+  h1 {
+    font-size: 2rem;
+    text-align: right;
+    color: var(--white-text);
+    margin-bottom: 1.5rem;
+  }
+
+  p {
+    font-size: 1rem;
+    margin-bottom: 1rem;
+    text-align: justify;
+    color: var(--gray-600-text);
+    @media (max-width: 768px) { font-size: 1rem; } // tablet props
+  }
 `
