228a637867e700abede7075de63374c1286b0f76
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-12 19:46:47 -0300

    feat: implements history component

--- src/modules/History/index.jsx
+++ src/modules/History/index.jsx
@@ -1,9 +1,37 @@
-import { ContainerStyle } from './styles'
+import ArrowRight from '../../assets/arrow_right.svg'
+import { HistoryContainer, Article, Links } from './styles'
 
 export default function History () {
   return (
-    <ContainerStyle>
-      History
-    </ContainerStyle>
+    <HistoryContainer>
+      <Article>
+        <h1>Na verdade, o que você precisa saber é que...</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit distinctio consequuntur deleniti vero commodi quas facere ipsum dicta ad cupiditate voluptate quidem quasi iste, rem dolore in, eius quis nemo?
+        </p>
+        <p>
+          Quo modi cum repudiandae, autem assumenda earum, mollitia sequi doloremque dolor dolorem iusto nemo veritatis excepturi fugit.
+          Possimus soluta atque aspernatur obcaecati! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore veritatis delectus numquam illum ullam voluptatibus obcaecati pariatur deserunt harum? Sed, architecto corporis rem natus ipsam accusamus at necessitatibus provident iure.
+          Minus itaque exercitationem aut ullam, accusantium reprehenderit veniam consectetur perspiciatis at unde sit architecto nisi! Nemo voluptatibus repudiandae cum accusantium! Fuga, sequi.
+        </p>
+        <p>
+          Possimus soluta atque aspernatur obcaecati! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </p>
+      </Article>
+      <Links>
+      <ul>
+        <li>
+          <a className="primary" href="">
+            <span>Tenha acesso</span> 
+            <img src={ArrowRight}/>
+          </a>
+          </li>
+      </ul>
+      </Links>
+    </HistoryContainer>
   )
 }
--- src/modules/History/styles.js
+++ src/modules/History/styles.js
@@ -1,5 +1,78 @@
 import styled from 'styled-components'
 
-export const ContainerStyle = styled.div`
+export const HistoryContainer = styled.div`
+  width: 100%;
 
+  padding: 4rem;
+  @media (max-width: 768px) { padding: 4rem 3rem; } // tablet props
+  @media (max-width: 425px) { padding: 3rem 2rem; } // mobile props
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  position: relative;
+`
+
+export const Article = styled.div`
+  padding: 0 4rem 0 0;
+  @media (max-width: 768px) { padding: 0 0 2rem 0; } // tablet props
+
+  h1 {
+    font-size: 2rem;
+    color: var(--white-text);
+    margin-bottom: 1.5rem;
+  }
+
+  p {
+    font-size: 1rem;
+    margin-bottom: 1rem;
+    text-align: justify;
+    color: var(--gray-600-text);
+    @media (max-width: 768px) { font-size: 1rem; } // tablet props
+  }
+`
+
+export const Links = styled.nav`
+  margin-top: 1rem;
+  width: 100%;
+
+  ul { display: flex; }
+
+  li {
+    list-style: none;
+
+    margin-left: 1rem;
+    :first-child { margin-left: 0rem; }
+  }
+
+  a {
+    font-size: 1rem;
+    font-weight: bold;
+    color: var(--primary);
+    text-decoration: none;
+
+    span {
+      padding-bottom: 0.2rem;
+      border-bottom: 1px solid var(--primary);
+      transition: all 0.3s ease;
+      margin-right: 0.5rem;
+    }
+
+    img {
+      height: 1rem;
+    }
+
+    :hover {
+      span {
+        padding-bottom: 0.4rem;
+        margin-right: 1rem;
+      }
+
+      img {
+        height: 1rem;
+      }
+    }
+  }
 `
