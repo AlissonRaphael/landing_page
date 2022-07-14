265c88eaf55f8fc0608c6a53985f3f5e62f88211
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-14 20:37:06 -0300

    feat: implement pitch component

--- src/modules/Pitch/index.jsx
+++ src/modules/Pitch/index.jsx
@@ -1,9 +1,65 @@
-import { ContainerStyle } from './styles'
+import SectionTitle from '../../components/SectionTitle'
+import Icon1 from '../../assets/product/icon1.svg'
+import Icon2 from '../../assets/product/icon2.svg'
+import Icon3 from '../../assets/product/icon3.svg'
+import Icon4 from '../../assets/product/icon4.svg'
+import Icon5 from '../../assets/product/icon5.svg'
+import Icon6 from '../../assets/product/icon6.svg'
+import { PitchContainer, Article, Card } from './styles'
+
+const data = [
+  {
+    title: 'Lorem ipsum',
+    icon: Icon1,
+    description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
+  },
+  {
+    title: 'Ipsum dolor',
+    icon: Icon2,
+    description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
+  },
+  {
+    title: 'Ipsum dolor',
+    icon: Icon3,
+    description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
+  },
+  {
+    title: 'Ipsum dolor',
+    icon: Icon4,
+    description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
+  },
+  {
+    title: 'Ipsum dolor',
+    icon: Icon5,
+    description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
+  },
+  {
+    title: 'Ipsum dolor',
+    icon: Icon6,
+    description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
+  }
+]
 
 export default function Pitch () {
   return (
-    <ContainerStyle>
-      Pitch
-    </ContainerStyle>
+    <PitchContainer>
+      <Article>
+        <h1>Com isso mãos, você poderá...</h1>
+        <ul>
+          {
+            data.map(({ title, icon, description }) => (
+              <Card>
+                <img src={icon}/>
+                <div>
+                  <h3>{title}</h3>
+                  <div>{description}</div>
+                </div>
+              </Card>
+            ))
+          }
+        </ul>
+      </Article>
+      <SectionTitle>produto</SectionTitle>
+    </PitchContainer>
   )
 }
--- src/modules/Pitch/styles.js
+++ src/modules/Pitch/styles.js
@@ -1,5 +1,62 @@
 import styled from 'styled-components'
 
-export const ContainerStyle = styled.div`
+export const PitchContainer = styled.div`
+  width: 100%;
+
+  padding: 4rem;
+  @media (max-width: 768px) { padding: 4rem 3rem; } // tablet props
+  @media (max-width: 425px) { padding: 3rem 2rem; } // mobile props
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  position: relative;
+`
+
+export const Article = styled.div`
+  h1 {
+    font-size: 2rem;
+    color: var(--white-text);
+    margin-bottom: 2rem;
+  }
+
+  ul {
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: 1fr 1fr;
+    @media (max-width: 425px) { grid-template-columns: 1fr; } // mobile props
+  }
+`
+
+export const Card = styled.li`
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+  border-radius: 0.4rem;
+  padding: 2.5rem;
+  display: flex;
+
+  background-color: var(--background-side-color);
+
+  img {
+    height: 5rem;
+    padding-right: 1.5rem;
+    opacity: 0.7;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+
+    h3 {
+      margin-bottom: 0.5rem;
+      color: var(--white-text);
+    }
+
+    div {
+      font-size: 0.7rem;
+      text-align: justify;
+    }
+  }
 
 `
