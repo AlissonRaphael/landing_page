e4d5717300b9266a8c80959d618b05ae1e670cb5
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-15 18:20:15 -0300

    refactor: change vsl

--- src/modules/VSL/index.jsx
+++ src/modules/VSL/index.jsx
@@ -1,27 +1,81 @@
 import { useState, useEffect } from 'react'
 import ReactPlayer from 'react-player'
 
-import Gif from '../../assets/gif.gif'
+import PreviewFile from '../../assets/video/preview.gif'
 
 import Volume from './SVG'
 import { VSLContainer, Player, ProgressionBar } from './styles'
 
+
 const URL = process.env.URL_VIDEO
+const PLAYERS_CONFIG = {
+  youtube: {
+    playerVars: {
+      controls: 0,
+      showinfo: 0,
+      modestbranding: 1,
+      disablekb: 1,
+      fs: 0,
+      rel: 0,
+    }
+  },
+  vimeo: {
+    playerOptions: {
+      controls: false,
+      byline: false,
+      title: false,
+      playsinline: false,
+      keyboard: false
+    }
+  },
+  wistia: {
+    options: {
+      controlsVisibleOnLoad: false,
+      fullscreenButton: false,
+      fullscreenOnRotateToLandscape: false,
+      playButton: false
+    }
+  }
+}
 
 export default function VSL () {
   const [init, setInit] = useState(false)
   const [play, setPlay] = useState(false)
+  const [interaction, setInteraction] = useState('Click')
+
+  useEffect(() => {
+    if (window.innerWidth <= 768)
+      setInteraction('Toque')
+  }, [])
 
   const handleClick = () => {
     setInit(true)
     setPlay(!play)
   }
 
+  const Film = () => {
+    return <div className="film" onClick={() => handleClick()}></div>
+  }
+
+  const Preview = () => {
+    return  <img className='thumbnail' src={PreviewFile}/>
+  }
+
+  const AudioButton = () => {
+    return <div className="button">
+      <div>
+        <span>O vídeo começou</span>
+        <Volume/>
+        <span>{interaction} para ouvir.</span>
+      </div>
+    </div>
+  }
+
   return (
     <VSLContainer>
       <Player>
-        <Film handleClick={handleClick}/>
-        { !init ? <Thumbnail/> : null }
+        <Film/>
+        { !init ? <Preview/> : null }
         { !play ? <AudioButton/> : null }
         <Video play={play}/>
       </Player>
@@ -29,87 +83,12 @@ export default function VSL () {
   )
 }
 
-function Film ({ handleClick }) {
-  return (
-    <div
-      className="film"
-      onClick={() => handleClick()}
-    >
-    </div>
-  )
-}
-
 function Video ({ play }) {
   const [duration, setDuration] = useState(0)
 
-  return (
-    <>
-      <ReactPlayer
-        className='video'
-        height={0}
-        width={0}
-        url={URL}
-        playing={play}
-        onDuration={(time) => setDuration(time)}
-        config={{
-          youtube: {
-            playerVars: {
-              controls: 0,
-              showinfo: 0,
-              modestbranding: 1,
-              disablekb: 1,
-              fs: 0,
-              rel: 0,
-            }
-          },
-          vimeo: {
-            playerOptions: {
-              controls: false,
-              byline: false,
-              title: false,
-              playsinline: false,
-              keyboard: false
-            }
-          },
-          wistia: {
-            options: {
-
-            }
-          }
-        }}
-      />
-      <ProgressionBar
-        duration={duration} 
-        isPlay={play}
-      />
-    </>
-  )
-}
-
-function Thumbnail () {
-  return (
-    <img
-      className='thumbnail'
-      src={Gif}
-    />
-  )
-}
-
-function AudioButton () {
-  const [interaction, setInteraction] = useState('click')
-
-  useEffect(() => {
-    if (window.innerWidth <= 768)
-      setInteraction('touch')
-  }, [])
-
-  return (
-    <div className="button">
-      <div>
-        <span>O vídeo começou</span>
-        <Volume/>
-        <span>{interaction === 'click' ? 'Click' : 'Toque'} para ouvir.</span>
-      </div>
-    </div>
-  )
+  return <>
+    <ReactPlayer className='video' height={0} width={0} url={URL} playing={play}
+      onDuration={(time) => setDuration(time)} config={PLAYERS_CONFIG}/>
+    <ProgressionBar duration={duration} isPlay={play}/>
+  </>
 }
