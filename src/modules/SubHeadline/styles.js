0d5908943cea291a44715bac81c0cdd56a613313
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-19 22:55:10 -0300

    refactor: change subhealine component

--- src/modules/SubHeadline/styles.js
+++ src/modules/SubHeadline/styles.js
@@ -3,6 +3,10 @@ import styled from 'styled-components'
 export const SubHeadlineStyle = styled.div`
   max-width: 1440px;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
 
   p {
     text-align: justify;
