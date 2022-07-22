2c282668b705806ec53b75c2a63549565c91caa9
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-21 23:31:38 -0300

    feat: initial implements in guarantee comp

--- src/modules/Guarantee/index.jsx
+++ src/modules/Guarantee/index.jsx
@@ -1,9 +1,46 @@
-import { ContainerStyle } from './styles'
+import Money from '../../assets/guarantee/money.svg'
+import Arrow from '../../assets/guarantee/arrow.svg'
+import ArrowOut from '../../assets/guarantee/arrow_out.svg'
+import Credit from '../../assets/guarantee/credit.svg'
+import Visa from '../../assets/guarantee/visa.svg'
+import Mastercard from '../../assets/guarantee/mastercard.svg'
+import Paypal from '../../assets/guarantee/paypal.svg'
+import Pix from '../../assets/guarantee/pix.svg'
+import { Container, GuaranteeContainer, Grid } from './styles'
+
+import Icon1 from '../../assets/product/icon1.svg'
+import Icon2 from '../../assets/product/icon2.svg'
+import Icon3 from '../../assets/product/icon3.svg'
+import Icon4 from '../../assets/product/icon4.svg'
+import Icon5 from '../../assets/product/icon5.svg'
+import Icon6 from '../../assets/product/icon6.svg'
 
 export default function Guarantee () {
   return (
-    <ContainerStyle>
-      Guarantee
-    </ContainerStyle>
+    <Container>
+      <GuaranteeContainer>
+        <Grid>
+          <img className="a" src={Money} />
+          <div className="b">Compra segura</div>
+          <img className="c" src={Arrow} />
+
+          <img className="d" src={Icon4} />
+          <div className="e">
+            <div></div>
+          </div>
+          <img className="f" src={Icon5} />
+
+          <img className="g" src={ArrowOut} />
+          <div className="h">
+            <img src={Credit} />
+            <img src={Visa}/>
+            <img src={Mastercard}/>
+            <img className="paypal" src={Paypal}/>
+            <img className="pix" src={Pix}/>
+          </div>
+          <img className="i" src={Money} />
+        </Grid>
+      </GuaranteeContainer>
+    </Container>
   )
 }
--- src/modules/Guarantee/styles.js
+++ src/modules/Guarantee/styles.js
@@ -1,5 +1,125 @@
 import styled from 'styled-components'
 
-export const ContainerStyle = styled.div`
+export const Container = styled.section`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  position: relative;
+
+  background: linear-gradient(110deg, var(--primary) 0%, var(--primary) 50%, var(--primary-darken) 50%, var(--primary-darken) 100%);
+  background-color: var(--primary);
+`
+
+export const GuaranteeContainer = styled.div`
+  margin: 2rem 0 2rem 0;
+  border-radius: 2rem;
+
+  max-width: 1240px;
+  width: 100%;
+
+  padding: 4rem;
+  @media (max-width: 768px) { padding: 7rem 4rem 4rem 4rem; } // tablet props
+  @media (max-width: 425px) { padding: 3rem 2rem 3rem 2rem; } // mobile props
+`
+
+export const Grid = styled.div`
+  border-radius: 1rem;
+  padding: 2rem;
+
+  display: grid;
+  grid-template-areas: "a b c"
+                       "d e f"
+                       "g h i";
+  grid-template-columns: 4% 92% 4%;
+  grid-template-rows: 10% 80% 10%;
+
+  background-color: var(--primary);
+  box-shadow: rgba(17, 17, 26, 0.3) 0px 8px 24px, rgba(17, 17, 26, 0.3) 0px 16px 56px, rgba(17, 17, 26, 0.2) 0px 24px 80px;
+
+  align-items: center;
+  justify-items: center;
+
+  .a {
+    grid-area: a;
+    height: 25px;
+  }
+
+  .b {
+    grid-area: b;
+    height: 25px;
+
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 0.8rem;
+    font-weight: bold;
+    color: black;
+  }
+
+  .c {
+    grid-area: c;
+    height: 25px;
+  }
+
+
+  .d {
+    grid-area: d;
+    height: 25px;
+  }
+
+  .e {
+    grid-area: e;
+    width: 400px;
+    height: 400px;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    div {
+      border-radius: 50%;
+      width: 200px;
+      height: 200px;
+
+      background-color: black;
+    }
+
+  }
+
+  .f {
+    grid-area: f;
+    height: 25px;
+  }
+
+
+  .g {
+    grid-area: g;
+    height: 25px;
+  }
+
+  .h {
+    grid-area: h;
+    width: auto;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      height: 30px;
+      margin-right: 0.8rem;
+
+      &.pix, &.paypal {
+        height: 20px;
+      }
+    }
+  }
+
+  .i {
+    grid-area: i;
+    height: 25px;
+  }
 
 `
