438e519bc602287f97222b4d39eec1b61ef0fce4
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-21 23:28:23 -0300

    refactor: implements pitch component

--- src/modules/Pitch/index.jsx
+++ src/modules/Pitch/index.jsx
@@ -1,41 +1,47 @@
-import SectionTitle from '../../components/SectionTitle'
 import Icon1 from '../../assets/product/icon1.svg'
 import Icon2 from '../../assets/product/icon2.svg'
 import Icon3 from '../../assets/product/icon3.svg'
 import Icon4 from '../../assets/product/icon4.svg'
 import Icon5 from '../../assets/product/icon5.svg'
 import Icon6 from '../../assets/product/icon6.svg'
-import { PitchContainer, Article, Card } from './styles'
+
+import { PitchContainer, Title, Text, Cards, Card } from './styles'
 
 const data = [
   {
     title: 'Lorem ipsum',
     icon: Icon1,
+    color: '#25D366',
     description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
   },
   {
     title: 'Ipsum dolor',
     icon: Icon2,
+    color: '#E4405F',
     description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
   },
   {
     title: 'Ipsum dolor',
     icon: Icon3,
+    color: '#1877F2',
     description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
   },
   {
     title: 'Ipsum dolor',
     icon: Icon4,
+    color: '#FF0000',
     description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
   },
   {
     title: 'Ipsum dolor',
     icon: Icon5,
+    color: '#4285F4',
     description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
   },
   {
     title: 'Ipsum dolor',
     icon: Icon6,
+    color: '#00B2FF',
     description: 'Praesentium similique ipsam nobis culpa voluptatum perspiciatis quia vitae inventore quod, laboriosam minima magni corporis, dicta nisi labore doloribus voluptas amet est.'
   }
 ]
@@ -43,23 +49,30 @@ const data = [
 export default function Pitch () {
   return (
     <PitchContainer>
-      <Article>
-        <h1>Com isso mãos, você poderá...</h1>
-        <ul>
-          {
-            data.map(({ title, icon, description }) => (
-              <Card>
-                <img src={icon}/>
-                <div>
-                  <h3>{title}</h3>
-                  <div>{description}</div>
-                </div>
-              </Card>
-            ))
-          }
-        </ul>
-      </Article>
-      <SectionTitle>produto</SectionTitle>
+      <Title>Através dessa solução, você poderá...</Title>
+
+      <Text>
+        <p>
+          Reprehenderit distinctio consequuntur deleniti vero commodi quas facere ipsum dicta ad cupiditate voluptate quidem quasi iste
+        </p>
+      </Text>
+
+      <Cards>
+        {
+          data.map(({ title, icon, color, description }, index) => (
+            <Card color={color}>
+              <i className="ball"></i>
+              <i className="vertical"></i>
+              <i className="horizontal"></i>
+              <img src={icon}/>
+              <div>
+                <h3><span>{title}</span><div className="detail"></div></h3>
+                <div>{description}</div>
+              </div>
+            </Card>
+          ))
+        }
+      </Cards>
     </PitchContainer>
   )
 }
--- src/modules/Pitch/styles.js
+++ src/modules/Pitch/styles.js
@@ -1,46 +1,101 @@
 import styled from 'styled-components'
 
 export const PitchContainer = styled.div`
+  max-width: 1440px;
   width: 100%;
 
-  padding: 4rem;
-  @media (max-width: 768px) { padding: 4rem 3rem; } // tablet props
-  @media (max-width: 425px) { padding: 3rem 2rem; } // mobile props
+  padding: 7rem 14rem 14rem 14rem;
+  @media (max-width: 768px) { padding: 7rem 4rem 4rem 4rem; } // tablet props
+  @media (max-width: 425px) { padding: 3rem 2rem 3rem 2rem; } // mobile props
 
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
 
   position: relative;
 `
 
-export const Article = styled.div`
-  h1 {
-    font-size: 2rem;
-    color: var(--white-text);
-    margin-bottom: 2rem;
-  }
+export const Title = styled.h1`
+  margin-bottom: 1.5rem;
+  max-width: 40rem;
+  text-align: center;
+  align-self: center;
 
-  ul {
-    display: grid;
-    gap: 2rem;
-    grid-template-columns: 1fr 1fr;
-    @media (max-width: 425px) { grid-template-columns: 1fr; } // mobile props
+  font-size: 2.4rem;
+  @media (max-width: 768px) { font-size: 1.8rem; } // tablet props
+  @media (max-width: 425px) { font-size: 1.5rem; } // mobile props
+
+  color: var(--white-text);
+`
+
+export const Text = styled.div`
+  max-width: 60%;
+  align-self: center;
+
+  p {
+    font-size: 1rem;
+    line-height: 1.35rem;
+    margin-bottom: 1.5rem;
+    color: var(--gray-600-text);
+    text-align: center;
+
+    @media (max-width: 768px) { font-size: 1rem; } // tablet props
   }
 `
 
+export const Cards = styled.ul`
+  display: grid;
+  grid-template-columns: 48% 48%;
+  gap: 4%;
+`
+
 export const Card = styled.li`
+  margin-bottom: 1rem;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
   border-radius: 0.4rem;
   padding: 2.5rem;
   display: flex;
+  align-items: center;
 
   background-color: var(--background-side-color);
+  position: relative;
+
+  .ball {
+    position: absolute;
+    top: calc(50% - 7.5px);
+    left: -7.5px;
+
+    width: 15px;
+    height: 15px;
+
+    background-color: var(--background-side-color);
+    transform: rotate(45deg);
+  }
+
+  .vertical, .horizontal {
+    background-color: var(--background-card-color);
+  }
+
+  .vertical {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 3px;
+    height: 15px;
+  }
+
+  .horizontal {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 15px;
+    height: 3px;
+  }
 
   img {
-    height: 5rem;
-    padding-right: 1.5rem;
+    height: 4.5rem;
+    padding-right: 2rem;
     opacity: 0.7;
   }
 
@@ -51,10 +106,20 @@ export const Card = styled.li`
     h3 {
       margin-bottom: 0.5rem;
       color: var(--white-text);
+      display: flex;
+      align-items: end;
+
+      .detail {
+        margin-left: 5px;
+        width: 5px;
+        height: 5px;
+        background-color: ${props => props.color ? props.color : 'white' };
+      }
     }
 
     div {
       font-size: 0.7rem;
+      line-height: 1rem;
       text-align: justify;
     }
   }
