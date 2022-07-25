26c7ce4b143422153138bb1ee151606be55a4652
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-24 22:23:26 -0300

    refactor: add new sections in app

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import Header from './components/Header'
 import Main from './components/Main'
 import Action from './components/Action'
 import Page from './components/Page'
+import { ContainerLineDetail } from './components/Detail'
 
 import Headline from './modules/Headline'
 import VSL from './modules/VSL'
 import SubHeadline from './modules/SubHeadline'
 import Lead from './modules/Lead'
 import History from './modules/History'
+import Pitch from './modules/Pitch'
+import Authority from './modules/Authority'
+import Depositions from './modules/Depositions'
+
+import Offer from './modules/Offer'
+import Guarantee from './modules/Guarantee'
 
 export default function App () {
   return (
@@ -20,13 +27,23 @@ export default function App () {
         <Headline/>
         <VSL/>
         <SubHeadline/>
-        <Action/>
+        <Action isMain={true}/>
       </Main>
 
       <Page>
+        <ContainerLineDetail/>
         <Lead/>
         <History/>
+        <Pitch/>
       </Page>
+
+      <Page>
+        <Authority/>
+        <Depositions/>
+      </Page>
+
+      <Offer/>
+      <Guarantee/>
 
       <GlobalStyle/>
     </>
