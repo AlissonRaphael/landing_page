8b3010d72edf1612c16aa1a39538d9cdd1ddbb69
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-07-24 22:18:39 -0300

    refactor: improves responsiveness of guarantee comp

--- src/modules/Guarantee/index.jsx
+++ src/modules/Guarantee/index.jsx
@@ -1,18 +1,17 @@
 import Money from '../../assets/guarantee/money.svg'
 import Arrow from '../../assets/guarantee/arrow.svg'
-import Credit from '../../assets/guarantee/credit.svg'
 import Visa from '../../assets/guarantee/visa.svg'
 import Mastercard from '../../assets/guarantee/mastercard.svg'
 import Paypal from '../../assets/guarantee/paypal.svg'
 import Pix from '../../assets/guarantee/pix.svg'
+
+import GuaranteeLogo from '../../assets/guarantee/guarantee.svg'
+import PrivacyLogo from '../../assets/guarantee/privacy.svg'
+import SecurityLogo from '../../assets/guarantee/security.svg'
+
+
 import { Container, GuaranteeContainer, Grid } from './styles'
 
-import Icon1 from '../../assets/product/icon1.svg'
-import Icon2 from '../../assets/product/icon2.svg'
-import Icon3 from '../../assets/product/icon3.svg'
-import Icon4 from '../../assets/product/icon4.svg'
-import Icon5 from '../../assets/product/icon5.svg'
-import Icon6 from '../../assets/product/icon6.svg'
 
 export default function Guarantee () {
   return (
@@ -23,20 +22,23 @@ export default function Guarantee () {
           <div className="b">Compra segura</div>
           <img className="c" src={Arrow} />
 
-          <img className="d"/>
-          <div className="e">
-            <div></div>
+          <div className="d">
+            <img className="security" src={SecurityLogo}/>
+            <div className="guarantee">
+              <img  src={GuaranteeLogo}/>
+              <span>07 dias</span>
+            </div>
+            <img className="privacy" src={PrivacyLogo}/>
           </div>
-          <img className="f" />
 
-          <img className="g" src={Arrow} />
-          <div className="h">
+          <img className="e" src={Arrow} />
+          <div className="f">
             <img src={Visa}/>
             <img src={Mastercard}/>
             <img className="paypal" src={Paypal}/>
             <img className="pix" src={Pix}/>
           </div>
-          <img className="i" src={Money} />
+          <img className="g" src={Money} />
         </Grid>
       </GuaranteeContainer>
     </Container>
--- src/modules/Guarantee/styles.js
+++ src/modules/Guarantee/styles.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
-import GuaranteeFile from '../../assets/guarantee/guarantee.svg'
+
 
 export const Container = styled.section`
+  margin: 0rem 0rem 7rem 0rem;
   width: 100%;
 
   display: flex;
@@ -21,19 +22,20 @@ export const GuaranteeContainer = styled.div`
   max-width: 1240px;
   width: 100%;
 
-  padding: 4rem;
-  @media (max-width: 768px) { padding: 7rem 4rem 4rem 4rem; } // tablet props
+  padding: 7rem 4rem;
+  @media (max-width: 1024px) { padding: 7rem 4rem 7rem 4rem; } // small desktop props
   @media (max-width: 425px) { padding: 3rem 2rem 3rem 2rem; } // mobile props
 `
 
 export const Grid = styled.div`
   border-radius: 1rem;
   padding: 2rem;
+  @media (max-width: 425px) { padding: 1rem; } // mobile props
 
   display: grid;
   grid-template-areas: "a b c"
-                       "d e f"
-                       "g h i";
+                       "d d d"
+                       "e f g";
   grid-template-columns: 4% 92% 4%;
   grid-template-rows: 10% 80% 10%;
 
@@ -64,44 +66,69 @@ export const Grid = styled.div`
     height: 20px;
   }
 
-
   .d {
     grid-area: d;
-    height: 25px;
+    width: 100%;
+    height: 100%;
+    padding: 5rem;
+    @media (max-width: 768px) { padding: 2rem; } // tablet props
+    @media (max-width: 425px) { padding: 0rem; } // mobile props
+
+
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    @media (max-width: 768px) { grid-template-columns: 1fr; } // tablet props
+    grid-template-rows: 1fr;
+    @media (max-width: 768px) { grid-template-rows: 1fr 1fr 1fr; } // tablet props
+    gap: 5%;
+  
+    align-items: center;
+    justify-items: center;
+    justify-content: center;
+
+    .security, .privacy {
+      width: 100%;
+      max-width: 160px;
+    }
+
+    .guarantee {
+      width: 100%;
+      height: 100%;
+      max-width: 250px;
+      position: relative;
+
+      img {
+        width: 100%;
+      }
+
+      span {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        color: black;
+        font-size: 2.2rem;
+        @media (max-width: 1024px) { font-size: 1.5rem; } // small desktop props
+        @media (max-width: 768px) { font-size: 2.2rem; } // tablet props
+        font-weight: bold;
+      }
+    }
   }
 
   .e {
     grid-area: e;
-    width: 400px;
-    height: 400px;
-    
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    div {
-      width: 300px;
-      height: 287px;
-      background-image: url(${GuaranteeFile});
-      background-size: cover;
-    }
-
-  }
-
-  .f {
-    grid-area: f;
-    height: 25px;
-  }
-
-
-  .g {
-    grid-area: g;
     height: 20px;
     transform: rotate(180deg);
   }
 
-  .h {
-    grid-area: h;
+  .f {
+    grid-area: f;
     width: auto;
 
     display: flex;
@@ -118,8 +145,8 @@ export const Grid = styled.div`
     }
   }
 
-  .i {
-    grid-area: i;
+  .g {
+    grid-area: g;
     height: 25px;
   }
 
