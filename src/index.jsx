24f1c430fa69fec609cc1ba78c5c3db4ff546751
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-08-06 14:20:02 -0300

    refactor: updates to client rendering

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
-render(<App/>, document.querySelector('#root'))
+const root = createRoot(document.querySelector('#root'))
+
+root.render(<App/>)
