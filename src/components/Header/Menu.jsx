00e53a97538c46f32012239e1cf53c46278dd9d4
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-08-07 20:55:36 -0300

    fix: remove mobile menu

---
+++ src/components/Header/Menu.jsx
@@ -0,0 +1,10 @@
+import { Links } from './styles'
+
+export default function Menu () {
+  return <Links>
+    <ul>
+      <li><a href="">Termos de Uso</a></li>
+      <li><a href="">Políticas de Privacidade</a></li>
+    </ul>
+  </Links>
+}
